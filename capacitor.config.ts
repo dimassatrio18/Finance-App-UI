509026c3f2cecab972f68658b689e66da2f59bca
Dimas Satrio <if20.dimassatrioekasaputro@mhs.ubpkarawang.ac.id>
2023-08-21 21:45:46 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Finance-App-UI',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
